ca69c46a02002ca51063345d44d37480f03b356d
Nelren-Business <nelren.business@gmail.com>
2026-03-30 03:07:47 -0300

    Tracking added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,25 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import partytown from '@astrojs/partytown';
+
 export default defineConfig({
   site: 'https://Nelren-Business.github.io',
   base: '/',
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   build: {
     inlineStylesheets: 'always', 
   },
+
+  integrations: [
+    partytown({
+      config: {
+        forward: ['dataLayer.push'], // Permite que o GTM rastreie eventos
+      },
+    }),
+  ],
 });
