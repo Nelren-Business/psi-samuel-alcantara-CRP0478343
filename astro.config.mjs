cd6b574eac01ae6aad1370f1639efe2b4bec66ea
Nelren-Business <nelren.business@gmail.com>
2026-04-08 18:52:50 -0300

    adjust partytown error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import partytown from '@astrojs/partytown';
 
 export default defineConfig({
   site: 'https://samuelalcantara.com.br/',
